e9532295e947b28911be19ae859cc6f476a06313
isaac <pavel.budantsev@yahoo.com>
2019-05-25 17:30:50 +0300

    added data gathering

--- src/app/research-creation/research-creation.component.ts
+++ src/app/research-creation/research-creation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/auth/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-research-creation',
@@ -7,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResearchCreationComponent implements OnInit {
 	private keywords: string[] = [];
+	private topic = "";
+	private description = "";
+	private appName = "";
+	private appDev = "";
+	private appId = "";
 
-	constructor() { }
+	constructor(private authService: AuthenticationService, private router: Router) {
+		if (!authService.isAuthenticated()) {
+			router.navigate(["/"]);
+		}
+	}
 
 	ngOnInit() {
 		document.getElementById("google-play-service")
@@ -27,6 +38,84 @@ export class ResearchCreationComponent implements OnInit {
 					target["value"] = "";
 				}
 			});
+	}
+
+	private gatherRequestData() : Object {
+		let requestData = {};
+		let modules = this.getModules();
+		requestData["topic"] = this.getTopic();
+		requestData["description"] = this.getDescription();
+		requestData["modules"] = this.getModules();
+		requestData["update_interval"] = this.getUpdateInterval();
+		requestData["isPublic"] = this.getResearchType() === "public";
+		requestData["analyzers"] = this.getAnalyzer();
+		requestData["keywords"] = this.keywords;
+		this.setPlayStoreParameters(requestData, modules);
+		return requestData;
+	}
+
+	submit() : void {
+		console.log(this.gatherRequestData());
+	}
+
+	private setPlayStoreParameters(requestData: Object, modules: string[]) : void {
+		if (!modules.includes("play_store")) {
+			requestData["app_name"] = requestData["app_id"] = requestData["app_dev"] = "";
+			return;
+		}
+		if (this.queryOptionByName("google-play-suboption", "play-name") === "play-name") {
+			requestData["app_name"] = this.appName;
+			requestData["app_dev"] = this.appDev;
+			requestData["app_id"] = "";
+		} else {
+			requestData["app_id"] = this.appId;
+			requestData["app_name"] = "";
+			requestData["app_dev"] = "";
+		}
+	}
+
+	private getTopic() : string {
+		return this.topic;
+	}
+
+	private getDescription() : string {
+		return this.description;
+	}
+
+	private getModules() : string[] {
+		let services = document.querySelectorAll("[data-service]");
+		let res: string[] = [];
+		for (let i = 0; i < services.length; i++) {
+			if (services[i]["checked"] === true) {
+				res.push(services[i]["value"]);
+			}
+		}
+		return res;
+	}
+
+	private getUpdateInterval() : string {
+		if (document.getElementById("use-auto-update")["checked"]) {
+			return this.queryOptionByName("auto-update-option", "");
+		}
+		return "";
+	}
+
+	private getAnalyzer() : string {
+		return this.queryOptionByName("analyzer", "vader");
+	}
+
+	private getResearchType() : string {
+		return this.queryOptionByName("research-type", "public");
+	}
+
+	private queryOptionByName(name: string, defaultOption: string) : string {
+		let options = document.querySelectorAll(`input[name="${name}"]`);
+		for (let i = 0; i < options.length; i++) {
+			if (options[i]["checked"]) {
+				return options[i]["value"];
+			}
+		}
+		return defaultOption;
 	}
 
 
